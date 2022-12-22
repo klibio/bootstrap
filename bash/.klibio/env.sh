40f0bd23a980aaa8a393a55e13040986c0fc29c1
Peter Kirschner <suprematic.kirschner@extaccount.com>
2022-12-22 08:12:32 +0100

    update env with exports

--- bash/.klibio/env.sh
+++ bash/.klibio/env.sh
@@ -3,14 +3,14 @@ set -Eeuo pipefail
 scriptDir=$(cd "$(dirname "$0")" && pwd)
 
 if [[ "$OSTYPE" == "msys" ]]; then
-  os=windows
-  jq=jq-win64.exe
+  export os=windows
+  export jq=jq-win64.exe
 fi
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  os=mac
-  jq=jq-osx-amd64
+  export os=mac
+  export jq=jq-osx-amd64
 fi
 if [[ "$OSTYPE" == "linux"* ]]; then
-  os=linux
-  jq=jq-linux64
+  export os=linux
+  export jq=jq-linux64
 fi
