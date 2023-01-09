81a01239231d2101e2b26dbf3a189c0fc213aa05
name <mailadress>
2023-01-09 15:41:23 +0100

    fix missing export of KLIBIO

--- pop.sh
+++ pop.sh
@@ -3,13 +3,6 @@ set -Eeuo pipefail
 
 GITHUB_REF_NAME=${1:-main}
 
-echo "# INSTALL"
-./install.sh -o -b=$GITHUB_REF_NAME
-
-echo "# DEBUG START"
-env | sort && ls -lar
-echo "# DEBUG START"
-
 function testEnvVar() {
     set +u
     declare var=$1
@@ -21,7 +14,15 @@ function testEnvVar() {
     set -u
 }
 
+echo "# INSTALL"
+./install.sh -o -b=$GITHUB_REF_NAME
+source "$HOME"/.bashrc
+
+echo "# DEBUG START"
+env | sort && ls -lar
+echo "# DEBUG START"
+
 echo "# test variable existences"
 
-testEnvVar KLIBIO
+estEnvVar KLIBIO
 testEnvVar JAVA_HOME
