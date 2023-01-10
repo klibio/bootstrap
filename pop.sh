2338e85f541e89e549c4c9e14526af5d431040b4
name <mailadress>
2023-01-10 09:02:16 +0100

    fix branch ref in github action

--- pop.sh
+++ pop.sh
@@ -18,7 +18,7 @@ echo "# INSTALL"
 curl -fsSL https://raw.githubusercontent.com/klibio/bootstrap/${branch}/install.sh > ./klibio_setup.sh
 chmod u+x ./klibio_setup.sh
 # for argument passing see https://unix.stackexchange.com/a/144519/116365
-bash -x ./klibio_setup.sh -b=${branch}
+bash -x ./klibio_setup.sh -b=${branch} -o
 rm klibio_setup.sh
 
 echo "# source the installed .bashrc file "
