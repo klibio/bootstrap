fa65d7501d401895a38a9e8f8c5f003626e1e79e
TheECanyon <schindler89@aol.com>
2023-01-10 13:30:06 +0100

    create tools archive, create proper unzip location

--- bash/.klibio/provideEclipseInstaller.sh
+++ bash/.klibio/provideEclipseInstaller.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-eclInstaller=""
 export KLIBIO=$HOME/.klibio
 toolsDir=$(realpath -s "$KLIBIO/tool")
 toolsArchives=$toolsDir/archives
+installerDir=$toolsDir/eclipse-installer
+
+mkdir -p $installerDir
+mkdir -p $toolsArchives
+
 source $KLIBIO/env.sh
 
-downloadUrl=https://download.eclipse.org/oomph/products/latest/eclipse-inst-${eclInstaller}
-outputFile=eclipse-inst-jre-${eclInstaller}
+downloadUrl=https://download.eclipse.org/oomph/products/latest/eclipse-inst-$eclInstaller
+outputFile=eclipse-inst-jre-$eclInstaller
 
 curl -sSL \
     $downloadUrl \
-    > $toolsDir$outputFile
+    > $toolsArchives/$outputFile
 
 if [[ $os == linux ]]; then
-    tar -zxvf "eclipse-inst-jre-linux64.tar.gz" -C "$toolsDir"
+    tar -zxvf "eclipse-inst-jre-linux64.tar.gz" -C "$installerDir"
 elif [[ $os == windows ]]; then
-    unzip -qq -d "$toolsDir" "$toolsArchives/$outputFile"
+    unzip -qq -d "$installerDir" "$toolsArchives/$outputFile"
 elif [[ $os == mac ]]; then
-    tar -zxvf "eclipse-inst-jre-linux64.tar.gz" -C "$toolsDir"
+    tar -zxvf "eclipse-inst-jre-linux64.tar.gz" -C "$installerDir"
 else
     echo -e "#\n# OS is none of linux/windows/mac. Aborting... \n#\n" && exit 1
 fi
