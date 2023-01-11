618c0af336f04955cb6342c353e56093ceecd903
name <mailadress>
2023-01-11 06:00:45 +0100

    fix java version testing

--- pop.sh
+++ pop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-set -Eeuo pipefail
+set -o nounset  # exit with error on unset variables
+set -o errexit  # exit if any statement returns a non-true return value
+set -o pipefail # exit if any pipe command is failing
 
 branch=${1:-main}
 
@@ -11,4 +13,4 @@ bash ./klibio_setup.sh -b=${branch} -o
 rm klibio_setup.sh
 
 echo "# launch a new bash with the actual test (sourcing the installed .bashrc) "
-bash -x ~/.klibio/testEnv.sh
+bash ~/.klibio/testEnv.sh
