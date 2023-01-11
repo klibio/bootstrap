fe21e3fb34d6e8795e286fdc5ab73968404d9138
name <mailadress>
2023-01-11 11:52:36 +0100

    fix test execution macosx

--- bash/.klibio/env.sh
+++ bash/.klibio/env.sh
@@ -3,20 +3,19 @@ if [[ "$OSTYPE" == "msys" ]]; then
   export os=windows
   export jq=jq-win64.exe
   export eclInstaller=win64.zip
+  export java_home_suffix=
 fi
 if [[ "$OSTYPE" == "darwin"* ]]; then
   export os=mac
   export jq=jq-osx-amd64
-<<<<<<< HEAD
   export java_home_suffix=/Contents/Home
-=======
   export eclInstaller=mac64.tar.gz
->>>>>>> f7473c7... Feature/eclipse installer (#2)
 fi
 if [[ "$OSTYPE" == "linux"* ]]; then
   export os=linux
   export jq=jq-linux64
   export eclInstaller=linux64.tar.gz
+  export java_home_suffix=
 fi
 
 echo "running on os $os"
