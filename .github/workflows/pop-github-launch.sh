378b9ead25f5824c13454ced7f04303a9b8210e4
name <mailadress>
2023-01-13 14:05:32 +0100

    single tool installation

--- .github/workflows/pop-github-launch.sh
+++ .github/workflows/pop-github-launch.sh
@@ -18,10 +18,10 @@ headline "proof-of-performance execution - started"
 headline "# execute users command - install-klibio.sh"
 curl -fsSLO https://raw.githubusercontent.com/klibio/bootstrap/${branch}/install-klibio.sh
 chmod u+x ./install-klibio.sh
-bash ./install-klibio.sh -b=${branch} -f -j -o
+bash -x ./install-klibio.sh -b=${branch} -f -j -o
 rm install-klibio.sh
 
 padout "# launch a new bash with the actual test (sourcing the installed .bashrc) "
-bash ~/.klibio/pop.sh
+bash -x ~/.klibio/pop.sh
 
 headline "proof-of-performance execution - finished"
