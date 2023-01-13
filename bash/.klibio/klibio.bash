8a1eb78d8985dd39e7ca848d4661b50321b8130b
name <mailadress>
2023-01-13 14:27:54 +0100

    single tool installation

--- bash/.klibio/klibio.bash
+++ bash/.klibio/klibio.bash
@@ -40,7 +40,7 @@ fi
 if [[ "$OSTYPE" == "darwin"* ]]; then
   export os=mac
   export jq=jq-osx-amd64
-  if [[ "arm64"  == "$(arch)" ]]; then
+  if [[ "$(arch)" == "arm"* ]]; then
     export oomph_suffix=mac-aarch64.tar.gz
   else
     export oomph_suffix=mac64.tar.gz
@@ -50,7 +50,7 @@ fi
 if [[ "$OSTYPE" == "linux"* ]]; then
   export os=linux
   export jq=jq-linux64
-  if [[ "arm64" == "$(arch)" ]]; then
+  if [[ "$(arch)" == "arm"* ]]; then
     export oomph_suffix=linux-aarch64.tar.gz
   else
     export oomph_suffix=linux64.tar.gz
