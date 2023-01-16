78e6d7022ed1755a3cf8563363258c4766209be4
Peter Kirschner <peter@kirschners.de>
2023-01-16 13:02:56 +0100

    fix exec rights

--- install-klibio.sh
+++ install-klibio.sh
@@ -4,7 +4,7 @@
 #
 
 # activate bash checks
-#set -o xtrace   # activate debug
+set -o xtrace   # activate debug
 set -o nounset  # exit with error on unset variables
 set -o errexit  # exit if any statement returns a non-true return value
 set -o pipefail # exit if any pipe command is failing
