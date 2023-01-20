7fbec06a635bc6d1ffeeaf9f28e02a746b111a25
name <mailadress>
2023-01-20 07:44:36 +0100

    oomph config prod osgi-dev and bootstrap

--- .github/workflows/pop-github-launch.sh
+++ .github/workflows/pop-github-launch.sh
@@ -27,8 +27,7 @@ headline "proof-of-performance execution - started"
 
 headline "# execute users command - install-klibio.sh"
 curl -fsSLO https://raw.githubusercontent.com/klibio/bootstrap/${branch}/install-klibio.sh
-chmod u+x ./install-klibio.sh
-bash ${activate_debug} ./install-klibio.sh -b=${branch} -f -j
+bash ${activate_debug} ./install-klibio.sh -b=${branch} -f -j -o
 rm install-klibio.sh
 
 padout "# launch a new bash with the actual test (sourcing the installed .bashrc) "
