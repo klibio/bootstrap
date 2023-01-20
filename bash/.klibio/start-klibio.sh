654a93ea6fc0256f64d9bb11af3e2671706fc787
Peter Kirschner <peter@kirschners.de>
2023-01-20 13:27:10 +0100

    Feature/oomph setups (#25)
    * remove chatty klibio.bash
    
    * allow oomph exec without arg
    
    * remove debugging in start-klibio.sh
    
    * allow oomph config selection configuration
    
    Co-authored-by: Peter Kirschner <peter@klib.io>

--- bash/.klibio/start-klibio.sh
+++ bash/.klibio/start-klibio.sh
@@ -23,6 +23,9 @@ oomph=0
 for i in "$@"; do
   case $i in
     # tool parameter
+    -o|--oomph)
+      oomph=1
+      ;;
     -o=*|--oomph=*)
       oomph_config="${i#*=}"
       oomph=1
@@ -30,7 +33,6 @@ for i in "$@"; do
       ;;
     # tool parameter
     --dev)
-      set -o xtrace
       dev_vm_arg="$(cat <<-EOM
 -Duser.home=x:/oomph_delme \
 -Doomph.setup.user.home.redirect=true
@@ -49,15 +51,24 @@ done
 
 # Command line argument for specifying a Configuration https://www.eclipse.org/forums/index.php/t/1086000/
 
-if (($oomph)); then
-  echo "# launching in separate window oomph with config ${branch}/${oomph_config/\//_}.setup"
-  config_url=https://raw.githubusercontent.com/klibio/bootstrap/${branch}/oomph/config/KlibioBoostrapConfiguration.setup
-  jvm=$(echo ~/.klibio/java/ee/JAVA11/bin)
-  ${KLIBIO}/tool/eclipse-installer/eclipse-inst.exe \
-    -vm ${jvm} \
-    ${config_url} \
-    -vmargs \
-      ${dev_vm_arg:-""} &
+if [[ -z $oomph ]]; then
+  # minimal oomph version
+  jvm=$(echo ${KLIBIO}/java/ee/JAVA11/bin)
+  if [[ -z ${oomph_config} ]];; then
+    echo "# launching oomph in separate window"
+    ${KLIBIO}/tool/eclipse-installer/eclipse-inst.exe \
+      -vm ${jvm} \
+      -vmargs \
+        ${dev_vm_arg:-""} &
+   else
+    config_url=https://raw.githubusercontent.com/klibio/bootstrap/${branch}/oomph/config/${oomph_config/\//_}.setup
+    echo "# launching oomph in separate window with config ${config_url}"
+    ${KLIBIO}/tool/eclipse-installer/eclipse-inst.exe \
+      -vm ${jvm} \
+      ${config_url} \
+      -vmargs \
+        ${dev_vm_arg:-""} &
+   fi
 fi
 
 set +o nounset  # exit with error on unset variables
---
+++ doc/user_stories.md
@@ -0,0 +1,24 @@
+# User Stories
+
+describe the usage from the perspective of the consuming user of this library
+
+## installation
+
+copy/paste and execute a one liner for installation (only bash and curl required)
+
+## usage
+
+### switching java versions
+* `set-java.sh <8,11,17>`
+
+### launching oomph eclipse installer
+
+* `start-klibio.sh -o` - launch oomph installer and select product and project
+
+* `start-klibio.sh -o <github klibio/repo>` - directly launch oomph installer with a specific oomph configuration of product and project
+e.g. `start-klibio.sh -o klibio/bootstrap`
+
+## principles
+
+* idempotent execution
+* 
