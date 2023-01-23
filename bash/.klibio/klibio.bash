06331a4b25accd4d7c648ba413486b76c1743b05
Peter Kirschner <peter@klib.io>
2023-01-23 08:27:46 +0100

    on overwrite delete previous file/folder

--- bash/.klibio/klibio.bash
+++ bash/.klibio/klibio.bash
@@ -132,13 +132,12 @@ download_file_from_github() {
 
 download_and_extract_file_from_github() {
     target_folder=${2:-~}
-    if [[ ${overwrite:-false} == true ]]; then rm -rf ${target_folder} >/dev/null 2>&1; fi
     branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null ) || branch=${branch:-main}
     url=https://raw.githubusercontent.com/klibio/bootstrap/${branch}/$1
     echo "downloading and extract into ${target_folder}"
     curl -sSL \
         ${url} \
-        | tar xvz -C ${target_folder} > /dev/null
+        | tar xvz --overwrite -C ${target_folder} > /dev/null
 }
 
 github_provision() {
