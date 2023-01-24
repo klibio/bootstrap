75d4c2e0957db0a6bca3f058efde4a3a1ef7026c
Peter Kirschner <peter@klib.io>
2023-01-24 14:38:22 +0100

    fix github tests

--- .github/workflows/pop-github-launch.sh
+++ .github/workflows/pop-github-launch.sh
@@ -2,30 +2,21 @@
 #
 # proof-of-performance execution
 #
-
+script_dir=$(dirname $(readlink -e $BASH_SOURCE))
+# activate bash checks
+if [[ ${debug:-false} == true ]]; then
+  set -o xtrace   # activate bash debug
+  export activate_debug=-x
+  echo "uname=$(uname -a)"
+  echo "arch=$(arch)"
+  echo "# DEBUG env start"
+  env | sort
+  echo "# DEBUG env end"
+fi
 # activate bash checks
 set -o nounset  # exit with error on unset variables
 set -o errexit  # exit if any statement returns a non-true return value
 set -o pipefail # exit if any pipe command is failing
-
-# activate debug
-if [[ ! -z ${debug+x} ]]; then 
-    echo "$(cat <<-EOM
-###########################################################
-#
-# DEBUG
-#
-###########################################################
-EOM
-)"
-    set -o xtrace
-    export activate_debug=-x
-    echo "uname=$(uname -a)"
-    echo "arch=$(arch)"
-    echo "# DEBUG env start"
-    env | sort
-    echo "# DEBUG env end"
-fi
 
 branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null ) || branch=${branch:-main}
 lib_url=https://raw.githubusercontent.com/klibio/bootstrap/${branch}/bash/.klibio/klibio.sh
