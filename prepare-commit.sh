20c31243090d60030c266336d67c7575dd9b8cf5
Peter Kirschner <peter@klib.io>
2023-01-25 21:36:14 +0100

    fix execution on mac

--- prepare-commit.sh
+++ prepare-commit.sh
@@ -2,7 +2,8 @@
 #
 # script for creating derived resources before a commit
 #
-script_dir=$(dirname $(readlink -e $BASH_SOURCE))
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # activate bash checks
 if [[ ${debug:-false} == true ]]; then
   set -o xtrace   # activate bash debug
