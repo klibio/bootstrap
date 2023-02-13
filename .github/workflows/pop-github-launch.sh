ade136aec5e036b8ffe5126404474cbd32572985
Peter Kirschner <peter@klib.io>
2023-02-13 09:05:47 +0100

    oomph redirection error

--- .github/workflows/pop-github-launch.sh
+++ .github/workflows/pop-github-launch.sh
@@ -19,7 +19,7 @@ set -o errexit  # exit if any statement returns a non-true return value
 set -o pipefail # exit if any pipe command is failing
 
 branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null ) || branch=${branch:-main}
-lib_url=https://raw.githubusercontent.com/klibio/bootstrap/${branch}/bash/.klibio/klibio.sh
+lib_url=https://raw.githubusercontent.com/klibio/bootstrap/${branch}/.klibio/klibio.sh
 $(curl -fsSLO ${lib_url})
 . klibio.sh
 
@@ -27,13 +27,10 @@ headline "proof-of-performance execution - started"
 
 installer_url=https://raw.githubusercontent.com/klibio/bootstrap/${branch}/install-klibio.sh
 headline "# execute users command - ${installer_url}"
-curl -fsSLO ${installer_url}
-bash ${activate_debug:-} ./install-klibio.sh -b=${branch} -f -j -o
-rm install-klibio.sh
+/bin/bash -c "$(curl -fsSLO ${installer_url})" bash -j -o -f
 
 headline "# launch pop inside new bash (containing installed .bashrc) "
 ls -la ${HOME}
-cat ${HOME}/.bashrc
-bash ${activate_debug:-} ${KLIBIO}/pop.sh
+${KLIBIO}/pop.sh
 
 headline "proof-of-performance execution - finished"
