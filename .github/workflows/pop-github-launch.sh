ef6017c64b3e9111e03089a7a370eebfdbdbf110
Peter Kirschner <peter@klib.io>
2024-04-29 07:04:40 +0200

    fixing pop

--- .github/workflows/pop-github-launch.sh
+++ .github/workflows/pop-github-launch.sh
@@ -23,7 +23,7 @@ echo -e "#\n# running on branch $branch\n#\n"
 
 installer_url=https://raw.githubusercontent.com/klibio/bootstrap/${branch}/install-klibio.sh
 echo -e "#\n# start # install klibio extension from ${installer_url}\n#\n"
-/bin/bash -x -c "$(curl -fsSLO ${installer_url})" bash -j -o -f
+/bin/bash -x -c $(curl -fsSLO ${installer_url} bash -j -o -f)
 echo -e "#\n# finished # install klibio extension\n#\n"
 
 echo -e "#\n# start # launching proof-of-performance\n#\n"
